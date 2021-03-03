b75d05df6c045a1010d6e6e4c529d64f28caf690
exw-eranda <eranda@fanaticzine.com>
2021-03-03 14:21:40 +0530

    create avenger jsx

---
+++ src/components/avenger.jsx
@@ -0,0 +1,14 @@
+import React, { Component, Fragment } from "react";
+
+class Avenger extends Component {
+    render() {
+        return (
+            <Fragment>
+                <h1>Avneger Incoming...</h1>
+                <button>Click Here</button>
+            </Fragment>
+        )
+    }
+}
+
+export default Avenger;
--- src/index.js
+++ src/index.js
@@ -1,17 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Avenger from "./components/avenger";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+// h1 tag using react
+// var firstElement = React.createElement("h1", null, "Hello world!") :- BABEL
+// const firstElement = <h1>Hello World!</h1> 
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+ReactDOM.render(<Avenger/>, document.getElementById("root"));
